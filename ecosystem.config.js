8cc032bd34256802b64e0c0881ccfdab980baf89
Monica A.L. Valdez <19965979+monicavaldez@users.noreply.github.com>
2025-05-09 18:32:51 +0800

    no precinct prompt

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,11 +15,12 @@ module.exports = {
         {
             name   : "caloocan2025-encoder",
             script: "npm",
-            args: "start start",
+            args: "run dev",
             cwd: './encoder',
             env: {
                 NODE_ENV: "production",
-                API_HOST: "http://localhost:8055"
+                API_HOST: "http://localhost:8055",
+                PATH: "$(asdf where nodejs)/bin:$PATH"
             }
         },
         {
