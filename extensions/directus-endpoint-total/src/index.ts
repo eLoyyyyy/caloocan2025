a69711682b3bde5e4afc317520c23ad928b03d74
Monica A.L. Valdez <19965979+monicavaldez@users.noreply.github.com>
2025-05-10 12:00:33 +0800

    top candidate per barangay

--- extensions/directus-endpoint-total/src/index.ts
+++ extensions/directus-endpoint-total/src/index.ts
@@ -24,6 +24,67 @@ export default defineEndpoint((router, { database, services, logger }) => {
   });
 
   router.get("/map-turnout", async (req, res) => {
-    
+    const info = await database.raw(`
+      WITH CandidateRegionalVotes AS (
+          SELECT
+              cd.surname || ', ' || cd.first_name AS candidate_name,
+              pr.psgc,
+              pr.barangay_name,
+              pr.registered_voters,
+              SUM(b.number_of_votes) AS total_votes,
+              RANK() OVER (PARTITION BY pr.psgc ORDER BY SUM(b.number_of_votes) DESC) AS region_rank,
+              p.position as position_name,
+              prt.party_color
+          FROM
+              "CANDIDATES_CALOOCAN" cd
+          JOIN
+              ballot b ON cd.id = b.candidate_id
+          JOIN
+              precincts pr ON b.precinct_id = pr.precinct_id
+          JOIN
+              "CANDIDATE_POSITION" p ON cd.position = p.id
+          JOIN
+              "PARTY" prt ON cd.party_id = prt.id
+          WHERE
+              p.position = 'Mayor'
+          GROUP BY
+              cd.surname,
+              pr.psgc,
+              p.position
+      ),
+      RankedTopCandidate AS (
+          SELECT
+              psgc,
+              barangay_name,
+              registered_voters,
+              candidate_name,
+              party_color,
+              total_votes,
+              region_rank,
+              MAX(CASE WHEN region_rank = 1 THEN total_votes ELSE -1 END) OVER (PARTITION BY psgc) AS top_vote_count_in_region
+          FROM
+              CandidateRegionalVotes
+      )
+      SELECT
+          psgc,
+          barangay_name,
+          registered_voters,
+          CASE
+              WHEN top_vote_count_in_region > 0 THEN candidate_name
+              ELSE 'No Candidate with Votes'
+          END AS top_candidate_name,
+          CASE
+              WHEN top_vote_count_in_region > 0 THEN party_color
+              ELSE '#808080'
+          END AS top_candidate_color
+      FROM
+          RankedTopCandidate
+      WHERE
+          region_rank = 1
+      ORDER BY
+          psgc;
+    `);
+
+    res.json(info);
   });
 });
